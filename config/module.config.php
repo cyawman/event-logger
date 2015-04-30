939ed5121a73ad5fb5d400933ddfa4b89d0f34ff
Yawman, Chris <christopher.yawman@neustar.biz>
2015-04-30 10:18:16 -0400

    Add factories to service manager config

--- config/module.config.php
+++ config/module.config.php
@@ -1,4 +1,12 @@
 <?php
 
 return array(
+    'service_manager' => array(
+        'factories' => array(
+            'EventLogger\Log\Logger' => 'EventLogger\Factory\Log\LoggerFactory',
+            'EventLogger\Log\Listener\AppListener' => 'EventLogger\Factory\Log\Listener\AppListenerFactory',
+            'EventLogger\Log\Listener\DispatchErrorListener' => 'EventLogger\Factory\Log\Listener\DispatchErrorListenerFactory',
+            'EventLogger\Log\Listener\ListenerAggregate' => 'EventLogger\Factory\Log\Listener\ListenerAggregateFactory'
+        )
+    )
 );
